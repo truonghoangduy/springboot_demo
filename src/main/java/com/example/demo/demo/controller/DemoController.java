774d5def4540e42ff089270aab760d53c8acbdb3
truonghoangduy <truonghoangduy03111999@gmail.com>
2021-12-18 00:06:21 +0700

    demo controller

--- src/main/java/com/example/demo/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/demo/controller/DemoController.java
@@ -2,21 +2,36 @@ package com.example.demo.demo.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class DemoController {
 
+  @GetMapping("/hello")
+  public String hello(
+      @RequestParam(name = "name", required = false, defaultValue = "") String name,
+      Model model
+  ) {
+    model.addAttribute("name", name);
+
+    return "hello";
+  }
+
+  @GetMapping("/hello/{date}")
+  public String helloPathVariable(
+      @PathVariable("date") String date,
+      @RequestParam(name = "name", required = false, defaultValue = "") String name,
+      Model model
+  ) {
+    model.addAttribute("date", date);
+    model.addAttribute("name", name);
+    return "hello";
+  }
+
   @GetMapping("/")
-  @ResponseBody
   public String helloWord() {
-    return "Hello world";
+    return "index";
   }
-
-  @PostMapping("/")
-  public String helloWordzzz() {
-    return "Hello world";
-  }
-
 
 }
